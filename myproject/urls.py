02a7adfc5a25e1bbe4eacfb757700406d2effbc5
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-06-15 10:38:52 +0545

    makes changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from myapp1.views import home
 
 urlpatterns = [
-    path("myapp/",include("myapp1.urls")),
+    path("",include("myapp1.urls")),
+    # path("home/",home),
     path('admin/', admin.site.urls)
 ]
