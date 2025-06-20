8d4e7cd42e3a8037e7ee35fda8022c43addafd4f
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-06-20 09:45:34 +0545

    makes changes

--- myapp1/views.py
+++ myapp1/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
+from .models import person
 # Create your views here.
 def home(request):
+    p = person.objects.all()
+    # dic = {
+    #     'name':"barsha",
+    #     'age':22
+    # }
     # return HttpResponse("hello world")
-    return render(request,"home.html")
+    # return render(request,"home.html",dic)
+    return render(request,"home.html",{'p':p})
+
+
